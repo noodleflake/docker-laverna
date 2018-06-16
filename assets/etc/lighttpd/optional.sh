8b0d096be744fb22e8d86370a6a95e565429d8d5
Swanepoel, Martin Peter : Barclays Africa Group Limited <Martin.Swanepoel@absa.co.za>
2018-06-16 21:17:20 +0200

    fix certs

--- assets/etc/lighttpd/optional.sh
+++ assets/etc/lighttpd/optional.sh
@@ -56,9 +56,9 @@ if [ -n "$LIGHTTPD_SSL_PEMFILE" ] && [ -r "$LIGHTTPD_SSL_PEMFILE" ]; then
 	echo "\$SERVER[\"socket\"] == \":6443\" {"
 	echo "server.use-ipv6 = \"disable\""
 	echo "ssl.engine = \"enable\""
-	echo "ssl.pemfile = \"/etc/lighttpd/ssl/$LIGHTTPD_SSL_PEMFILE\""
+	echo "ssl.pemfile = \"$LIGHTTPD_SSL_PEMFILE\""
 	if [ -n "$LIGHTTPD_SSL_CAFILE" ] && [ -r "$LIGHTTPD_SSL_CAFILE" ]; then
-		echo "ssl.ca-file = \"/etc/lighttpd/ssl/$LIGHTTPD_SSL_CAFILE\""
+		echo "ssl.ca-file = \"$LIGHTTPD_SSL_CAFILE\""
 	fi
 	echo "}"
 fi
